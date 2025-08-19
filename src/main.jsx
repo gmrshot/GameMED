2aebc83d4d716f4b8318a4eb487e9acce14ef937
Aizaz H <gmrshot@gmail.com>
2025-08-19 01:51:28 -0400

    Render GameMED from pages/Game.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Game from './pages/Game'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GameMED from "./pages/Game.jsx"; // <-- render your new file
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Game />
-  </React.StrictMode>,
-)
+    <GameMED />
+  </React.StrictMode>
+);
